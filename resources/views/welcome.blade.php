4c4a28c4cfed531ab4403e1b57138ae132d2a33a
abghithorr <abghithoriqal-baarsuryadi@smkwikrama.sch.id>
2023-05-11 11:02:06 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity ="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+<body>
+   <!--Awal Navbar -->
+   <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+        <img src="assets/img/logowikrama.png" width="50" height="50">
+
+          </a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">HOME</a>
+          </li>
+        </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">SHOP</a>
+            </li>
+          </a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">CONTACT</a>
+              </li>
+          </ul>
+        </div>
+    </div>
+  </nav>
+<!--Akhir Navbar-->
+
+<!--background-->
+<section class="jumbotron">
+  <div class="row justify-content-center align-items-center">
+    <div class="col-6">
+      <h1>Welcome to Caffe Wikrama!</h1>
+      <h2>Fresh and Delicious</h2>
+      <a type="button" href="/views" class="btn btn-primary btn-sm">Order Now</a>
+      <button type="button" class="btn btn-secondary btn-sm">Food Menu</button>
+    </div>
+    <div class="col-4">
+      <img src="https://img.freepik.com/free-vector/italian-cuisine-illustration_1284-37909.jpg?w=740&t=st=1678336303~exp=1678336903~hmac=bfadea73c4a35158a73933a00998e3b9f888be1ae4c55dbb2badc85f05a4e019" 
+    width="400px" >
+    </div>
+    <hr class="solid">
+    <div class="col-4">
+      <img src="https://img.freepik.com/free-vector/dalgona-coffee-illustration_23-2148583172.jpg?w=740&t=st=1678340606~exp=1678341206~hmac=020e2d1dd59fd321c078aabcfe89c3da1803ea7e8600063d32205b47d4fd82c2" 
+    width="400px" >
+    </div>
+    <div class="col-8">
+      <h1>Coffe Dalgona</h1>
+      <h5>a drink made by mixing equal parts instant coffee, sugar and hot water. The mixture is then whipped until it becomes creamy and then added to cold or hot milk.</h5>
+      <button type="button" class="btn btn-secondary">Rp12.000</button>
+    </div>
+    <hr class="solid">
+    <div class="col-6">
+      <h1>Friench Fries</h1>
+      <h5>Potatoes are cut into small lengths and fried in hot oil</h5>
+      <button type="button" class="btn btn-secondary">Rp8.000</button>
+    </div>
+    <div class="col-4">
+      <img src="https://img.freepik.com/free-vector/french-fries-paper-box-monochrome-fast-food-ad-restaurant-wall-decoration_460848-15603.jpg?w=740&t=st=1678339594~exp=1678340194~hmac=2684b587aa9b11e9c8b744b24188078c5888684a8b2cb35942b844763880c04a" 
+    width="400px" >
+  </div>
+  <hr class="solid">
+  <div class="col-4">
+    <img src="https://i.pinimg.com/564x/8c/f9/8b/8cf98b7b0a2f92c8afb22bf16f9cbd27.jpg" 
+     width="400px" >
+  </div>
+  <div class="col-8">
+    <h1>Boba</h1>
+    <h5>chewy tapioca balls, usually mixed with brown sugar or brown sugar to make it taste sweet and have a black color. Boba, which is also called "pearl" balls, is a drink made from tapioca flour with the addition of tea and milk</h5>
+    <button type="button" class="btn btn-secondary">Rp9.000</button>
+  </div>
+  <section style="background-color: #FFFFFF">
+    <div class="container my-5">
+        <div class="row text-center">
+    <h2 class="mt-2"><strong>Pembayaran</h2></strong>
+  <hr class="solid">
+  <div class="card-group">
+    <div class="card">
+      <img src="https://img.freepik.com/free-vector/cash-delivery-concept_23-2148798392.jpg?w=900&t=st=1678420047~exp=1678420647~hmac=02a6efe646c52e476d169ab9cadce0383eb09173d414d0a2146042a6fb39ed24" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"><strong>Cash</h5></strong>
+        <button type="button" class="btn btn-primary"
+        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+        Cash Click
+        </button>
+       </div>
+      <div class="card-footer">
+        <small class="text-muted">08.00-12.00</small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="https://img.freepik.com/free-photo/3d-render-money-transfer-mobile-banking-online_107791-16729.jpg?size=626&ext=jpg&ga=GA1.1.1852420019.1678332642&semt=ais" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"><strong>Credit</h5></strong>
+        <button type="button" class="btn btn-primary"
+        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+        Credit Click
+        </button>
+       </div>
+      <div class="card-footer">
+        <small class="text-muted">12.00-16.00</small>
+      </div>
+    </div>
+    <div class="card">
+      <img src="https://img.freepik.com/free-photo/online-payment-security-concept-3d-phone-bill_107791-16722.jpg?size=626&ext=jpg&ga=GA1.1.1852420019.1678332642&semt=ais" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"><strong>Transaksi</h5></strong>
+        <button type="button" class="btn btn-primary"
+        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+        Transaksi Click
+        </button>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">16.00-20.00</small>
+      </div>
+    </div>
+  </div>
+
+
+  
+</section>
+  
+
+</body>
+</html>
